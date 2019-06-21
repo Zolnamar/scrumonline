efa28aa7f58a424822a315175b3e43e3e2d150e5
Toxantron <thomas.fuchs91@gmail.com>
2019-06-21 15:05:10 +0200

    Master component implementation

--- src/app/master/master.component.ts
+++ src/app/master/master.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Session } from '../session';
+import { Card, MemberVote } from '../card';
 
 @Component({
   selector: 'app-master',
@@ -7,9 +10,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MasterComponent implements OnInit {
 
-  constructor() { }
+  session: Session = new Session();
+
+  votes: MemberVote[] = [];
+
+  stopwatchElapsed: string = '00:00';
+
+  teamComplete: boolean = false;
+
+  flipped: boolean;
+
+  constructor(
+    private route: ActivatedRoute) { 
+
+    }
 
   ngOnInit() {
+    this.session.id = +this.route.snapshot.paramMap.get('id');
+    this.session.name = "Hi";
+    this.flipped = true;
+    this.votes = [
+      {id: 1, value: '1', active: false,name: 'Thomas',confirmed: true,placed: true, canDelete: true},
+      {id: 1, value: '3', active: false,name: 'Sandra',confirmed: true,placed: true, canDelete: true},
+    ]
   }
 
+  joinUrl(encode: boolean) : string {
+    var location = window.location;
+    // Build url from location
+    var url = `${location.protocol}//${location.hostname}:${location.port}/join/${this.session.id}`;
+    if (encode)
+      url = encodeURIComponent(url);
+    return url;
+  }
+
+  removeMember(card: Card) {
+
+  }
+
+  wipe() {
+
+  }
 }
